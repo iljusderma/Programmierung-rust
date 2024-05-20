ced45b9db7c720e1f062da7e0a3a0ba4ac08761d
iljusderma <il2014@gmx.net>
2024-05-20 18:30:19 +0200

    finished exercise3 - serde

--- exercise3/src/main.rs
+++ exercise3/src/main.rs
@@ -1,30 +1,96 @@
+use std::fmt::write;
 use std::fs::File;
 use std::io::BufReader;
-use serde::Deserialize;
+use std::io::BufWriter;
+use serde::{Serialize, Deserialize};
 use std::error::Error;
 
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
 struct Author {
     name: String,
-    birthYear: usize,
+    #[serde(rename = "birthYear")]
+    birth_year: usize,
 }
 
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
 struct Book {
-    r#type: String,
     title: String, 
     year: usize,
     author: Author
 }
 
-#[derive(Deserialize, Debug, Clone)]
-enum Item {
-    Book {r#type:String, title:String, year:usize, author}
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+struct Newspaper {
+    title: String,
+    date: String,
 }
 
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+struct Movie {
+    title: String,
+    year: usize,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+#[serde(tag = "type")]
+enum Item {
+    Book(Book),
+    Newspaper(Newspaper),
+    Movie(Movie)
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Library {
     items: Vec<Item>,
+}
+
+impl Library {
+    fn add_item(&mut self, item: Item) -> &mut Self{
+        self.items.push(item);
+        self
+    }
+}
+
+impl Author {
+    fn new(name:String, birth_year:usize) -> Self {
+        Self {
+            name,
+            birth_year
+        }
+    }
+}
+
+impl Book {
+    fn new(title:String, year:usize, author:Author) -> Self {
+        Self {
+            title,
+            year,
+            author
+        }
+    }
+}
+
+impl Newspaper {
+    fn new(title:String, date:String) -> Self {
+        Self {
+            title,
+            date,
+        }
+    }
+}
+
+impl Movie {
+    fn new(title:String, year:usize) -> Self {
+        Self {
+            title,
+            year,
+        }
+    }
 }
 
 fn read_file() -> Result<Library, Box<dyn Error>> {
@@ -34,7 +100,31 @@ fn read_file() -> Result<Library, Box<dyn Error>> {
     Ok(library)
 }
 
+fn write_file(library:&Library) -> Result<(), Box<dyn Error>>{
+    let file = File::create("edited_library.json").unwrap();
+    let mut buf_writer = BufWriter::new(file);
+    serde_json::to_writer_pretty(&mut buf_writer, library);
+    Ok(())
+}
+
 fn main() {
-    let library = read_file().unwrap();
+    let mut library = read_file().unwrap();
     println!("{:#?}", library);
+    
+    // initialize new items
+    let author = Author::new(
+        "Neal Schustermann".to_string(), 
+        1962);
+    let book = Book::new(
+        "Scythe".to_string(), 
+        2016, 
+        author);
+    let movie = Movie::new("Nemo".to_string(), 2003);
+    let newspaper = Newspaper::new("Zeit".to_string(), "19.05.2024".to_string());
+
+    // Add new items to library
+    library.add_item(Item::Book(book));
+    library.add_item(Item::Newspaper(newspaper));
+    library.add_item(Item::Movie(movie));
+    write_file(&library);
 }
