eba285c2c1272a1babb56dc9e8fa9f50a4aec76f
iljusderma <il2014@gmx.net>
2024-05-22 00:09:38 +0200

    begin exercise 3b

---
+++ exercise3b/target/debug/deps/exercise3b-7fb1507f17183973.d
@@ -0,0 +1,5 @@
+/home/ilja/ownCloud/Studium/Semester_8/Programmierung/Programmierung-rust/exercise3b/target/debug/deps/libexercise3b-7fb1507f17183973.rmeta: src/main.rs
+
+/home/ilja/ownCloud/Studium/Semester_8/Programmierung/Programmierung-rust/exercise3b/target/debug/deps/exercise3b-7fb1507f17183973.d: src/main.rs
+
+src/main.rs:
---
+++ exercise3b/target/debug/deps/exercise3b-e01216860615184d.d
@@ -0,0 +1,5 @@
+/home/ilja/ownCloud/Studium/Semester_8/Programmierung/Programmierung-rust/exercise3b/target/debug/deps/libexercise3b-e01216860615184d.rmeta: src/main.rs
+
+/home/ilja/ownCloud/Studium/Semester_8/Programmierung/Programmierung-rust/exercise3b/target/debug/deps/exercise3b-e01216860615184d.d: src/main.rs
+
+src/main.rs:
