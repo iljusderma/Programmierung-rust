83e2ef92a32f9edee001b73181129494fd81d8ab
Ilja List <il2014@gmx.net>
2024-05-31 10:08:09 +0200

    fixed rust-analyzer error

---
+++ exercise3a/target/debug/deps/exercise3a-824cd52b610576ef.d
@@ -0,0 +1,5 @@
+/home/ilja/Documents/coding/Programmierung-rust/exercise3a/target/debug/deps/libexercise3a-824cd52b610576ef.rmeta: src/main.rs
+
+/home/ilja/Documents/coding/Programmierung-rust/exercise3a/target/debug/deps/exercise3a-824cd52b610576ef.d: src/main.rs
+
+src/main.rs:
---
+++ exercise3a/target/debug/deps/exercise3a-972f6012ea12abc4.d
@@ -0,0 +1,5 @@
+/home/ilja/Documents/coding/Programmierung-rust/exercise3a/target/debug/deps/libexercise3a-972f6012ea12abc4.rmeta: src/main.rs
+
+/home/ilja/Documents/coding/Programmierung-rust/exercise3a/target/debug/deps/exercise3a-972f6012ea12abc4.d: src/main.rs
+
+src/main.rs:
